2b8887787f7196548a0dbb236bbd9c2cfd3ac780
Arif Hossain <arif.raj9911@gmail.com>
2024-07-03 13:04:54 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
 
   return (
     <>
+      <div className='max-w-[1160px] mx-auto'>
       <Header></Header>
+      <Banner></Banner>
+      </div>
     </>
   )
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,9 @@
-
+import banner from '../../assets/Image/Rectangle 5.png'
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner</h2>
+        <div className='my-10'>
+            <img src={banner} alt="" />
         </div>
     );
 };
